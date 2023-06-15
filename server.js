cd52117dd186a52f4987f23062fc5388b47cfc83
Eliane <elianebettocchi@gmail.com>
2023-06-14 21:08:07 -0400

    fix

--- server.js
+++ server.js
@@ -4,51 +4,27 @@ const cors = require('cors')
 const mongoose = require('mongoose');
 require('dotenv').config();
 const PORT = process.env.PORT || 5000;
-
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true, })
-
 // Middleware
 app.use(cors());
 app.use(express.json());
-
-// view engine
-const connection = mongoose.connection;
-connection.once('open', () =>{
-    console.log("MongoDB database connection established succesfully");
-})
-
 // Routes
-// const artsRouter = require('../backend/routes/arts')
-// const familyRouter = require('../backend/routes/family')
-// const foodRouter = require('../backend/routes/food')
-// const historyRouter = require('../backend/routes/history')
-// const outdoorsRouter = require('../backend/routes/outdoors')
-// const sportsRouter = require('../backend/routes/sports')
-   const attractionsRouter = require('./routes/attractions')
-
-// app.use('/arts', artsRouter)
-// app.use('/family', familyRouter)
-// app.use('/food', foodRouter)
-// app.use('/history', historyRouter)
-// app.use('/outdoors', outdoorsRouter)
-// app.use('/sports', sportsRouter)
-   app.use('/attractions', attractionsRouter) 
-
-// app.get('*', checkUser);
-// app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
-// app.use(authRoutes);
-
+const attractionsRouter = require('./routes/attractions')
+app.use('/attractions', attractionsRouter)
+app.get('/', (req, res) => res.render('home'));
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
 app.use(function(req, res, next) {
-   res.header("Access-Control-Allow-Origin", "*");
-   next();
- });
-
-if(process.env.NODE_ENV === 'production'){
-   app.use(express.static('client/build'))
- }
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+// Connect to MongoDB
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true, })
+.then((result) => app.listen(3000))
+.catch((err) => console.log(err));
+const connection = mongoose.connection;
+connection.once('open', () =>{
+    console.log("MongoDB database connection established succesfully");
+})
